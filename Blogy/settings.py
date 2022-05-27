7ea96e1776cfd34e5c324cf556061470f4e07a43
mokhelek <mkatleho191@gmail.com>
2022-05-27 22:18:46 +0200

    added an offcanvas navbar

--- Blogy/settings.py
+++ Blogy/settings.py
@@ -97,6 +97,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
