f09f39d5c8fed933b53874e51d7a25c277664f71
mokhelek <mkatleho191@gmail.com>
2022-06-22 18:09:09 +0200

    fD

--- users/models.py
+++ users/models.py
@@ -5,15 +5,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    #name = models.OneToOneField(User, on_delete=models.CASCADE) # this has the username inside
-    #bio = models.CharField(max_length=300,blank=True, default="Hey There I'm using Bloggy")
-    #avatar = models.ImageField(null=True, upload_to="images", default="default.png")
+    name = models.OneToOneField(User, on_delete=models.CASCADE ) # this has the username inside
+    bio = models.CharField(max_length=300,blank=True, default="Hey There I'm using Bloggy")
+    avatar = models.ImageField(null=True, upload_to="images", default="default.png")
     
-    #following = models.ManyToManyField(User,related_name="following", blank=True)
+    following = models.ManyToManyField(User,related_name="following", blank=True)
     
-    #followers = models.IntegerField(default=0)
+    followers = models.IntegerField(default=0)
     
-    #date_created = models.DateField(auto_now=True)
+    date_created = models.DateField(auto_now=True)
     
     def profiles_posts(self):
         return self.entry_set.all()
