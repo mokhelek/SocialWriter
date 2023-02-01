9222d2b55d7c727c3e7edd1035b8dfe531e0cd1f
mokhelek <mkatleho191@gmail.com>
2023-02-02 00:39:01 +0200

    fixed bug with delete

--- blogyapp/signals.py
+++ blogyapp/signals.py
@@ -1,5 +1,5 @@
 
-from django.db.models.signals import post_save, m2m_changed , post_delete
+from django.db.models.signals import post_save, m2m_changed , post_delete , pre_delete
 from django.contrib.auth.models import User
 from .models import *
 from users.models import *
@@ -17,18 +17,19 @@ post_save.connect(comment_notification, sender=Comments )
 
 
 def delete_like_notification(sender, instance, **kwargs):
-    
-    Notification.objects.filter(reactor_profile = instance.profile , entry = instance.entry ).delete()
-       
+    try:
+        Notification.objects.filter(reactor_profile = instance.profile , entry = instance.entry ).delete()    
+    except:
+        pass
 post_delete.connect(delete_like_notification, sender=Like )
 
 
-def delete_bookmark_notification(sender, instance, **kwargs):
-    
-    Notification.objects.filter(reactor_profile = instance.profile , entry = instance.entry ).delete()
-       
+def delete_bookmark_notification(sender, instance, **kwargs):  
+    try:
+        Notification.objects.filter(reactor_profile = instance.profile , entry = instance.entry ).delete()     
+    except:
+        pass
 post_delete.connect(delete_bookmark_notification, sender=Bookmark )
-
 
 
 
@@ -53,7 +54,6 @@ def bookmark_notification(sender, instance, created,*args, **kwargs):
                                 type = "bookmark"
                                     )
         
-
 post_save.connect(bookmark_notification, sender=Bookmark )
 
 
@@ -84,8 +84,6 @@ def following_notification(sender, instance, action,pk_set ,*args, **kwargs):
         profiles = Profile.objects.get(name = instance.following.get(id = user_id[0] ) )
         
         Notification.objects.filter(profile = profiles , reactor_profile = instance ).delete()
-
-            
 
 m2m_changed.connect(following_notification, sender=Profile.following.through )
 
